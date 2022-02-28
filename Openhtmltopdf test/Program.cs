13d48891cea13a4d54ea77c19c8da39673fb6972
Mameik <mameik@ttu.ee>
2022-02-28 11:29:17 +0200

    Add project files.

---
+++ Openhtmltopdf test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace OpenHtmlToPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = File.ReadAllText("leping.html");
+
+            var pdf = OpenHtmlToPdf.Pdf
+                .From(html)
+                .Content();
+
+            File.WriteAllBytes("output.pdf", pdf);
+        }
+    }
+}
+
